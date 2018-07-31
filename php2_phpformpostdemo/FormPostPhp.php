a06c6e40a55103f5b886cc12dbf9678cc67fe502
2807738720@qq.com <2807738720@qq.com>
2018-07-31 13:14:36 +0800

    php学习代码提交

---
+++ php2_phpformpostdemo/FormPostPhp.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+</head>
+
+<body>
+
+<form action="FormShowPhp.php" method="post">
+
+    name:<input type="text" name="user_name"/>
+    from:<input type="text" name="address">
+    <input type="submit" value="post">
+
+</form>
+
+</body>
+
+</html>
+
+
---
+++ php2_phpformpostdemo/FormShowPhp.php
@@ -0,0 +1,6 @@
+大家好，我是<?php header("Content-Type:text/html;charset=utf-8");
+echo $_POST['user_name']; ?>!
+<?php echo "<br/>" ?>
+<?php echo "<br/>" ?>
+我来自<?php header("Content-Type:text/html;charset=utf-8");
+echo $_POST['address']; ?>
